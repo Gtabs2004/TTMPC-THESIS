a21b4658b731fc11a737609b720b08beb4911b45
Ervine11 <nashervine.siaton@wvsu.edu.ph>
2026-02-09 01:40:48 +0800

    login progress(nag crash laptop kooo)

--- TTMPC_THESIS/src/Index_Pages/login.jsx
+++ TTMPC_THESIS/src/Index_Pages/login.jsx
@@ -1,9 +1,73 @@
 import React from 'react'
 
- function login() {
+ function Login() {
   return (
-    <div>login</div>
+    <>
+    <div className="bg-[#E9F7DE] w-full h-20 flex items-center justify-between px-6">
+      <img src="src/assets/img/ttmpc logo.png" alt="Your Company" className="ml-12  p-2 h-16 w-auto" />
+      <nav>
+        <ul className="flex gap-8 items-center text-sm font-medium mr-4 cursor-pointer">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Products</li>
+          <li>FAQs</li>
+          <li>Contact Us</li>
+          <li>
+            <button className="border-green-400 text-green-400 py-2 border rounded p-2">Be a Member</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <form className=" mt-16 h-4/6 w-25 bg-white p-6 rounded-xl  shadow-md sm:mx-auto sm:w-full sm:max-w-sm">
+    <img src="src/assets/img/ttmpc logo.png" alt="Your Company" className="block mx-auto h-32 w-auto" />
+    <h2 className="text-center font-semibold text-xl">Login to your Account</h2>
+    <p className="text-center text-xs">Enter your details to login your account</p>
+
+        <label htmlFor="username"  className="block text-sm font-medium text-gray-700 ">
+  
+        </label>
+        <input
+          id="username"
+          name="username"
+          type="text"
+          placeholder ="Username"
+          autoComplete="username"
+          required
+          className=" border border-black block w-full rounded-md  shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 mt-1"
+        />
+        <label htmlFor="password"  className="block text-sm font-medium text-gray-700 mt-4">
+          
+        </label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          placeholder ="Password"
+          autoComplete="current-password"
+          required
+          className="border border-black block w-full rounded-md  shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 mt-1"
+        />
+        <span className="text-xs text-green-400 mt-2 block float-right mb-4">Forgot Password?</span>
+
+        <label htmlFor="role" className="block text-sm font-medium text-gray-700 mt-4">
+      
+        </label>  
+        <select
+          id="role"
+          name="role"
+          placeholder ="Role"
+          required
+          className="border border-black block w-full rounded-md  shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm p-2 mt-1">
+          <option value="">Select Role</option>
+          <option value="admin">Bookkeepr</option>
+          <option value="user">User</option>
+        </select>
+        <button>Back</button>
+        <button>Login</button>
+    </form>
+    </>
   )
 }
-export default login;
+export default Login;
 
--- TTMPC_THESIS/src/Router.jsx
+++ TTMPC_THESIS/src/Router.jsx
@@ -4,6 +4,7 @@ import Bonus_Loan from "./LOANFORMS/Bonus_Loan";
 import Consolidated_Loan from "./LOANFORMS/Consolidated_Loan";
 import Emergency_Loan from "./LOANFORMS/Emergency_Loan";
 import Sign_In from "./Index_Pages/Sign_in";
+import Login from "./Index_Pages/login";
 
 
 export const router = createBrowserRouter([
@@ -12,5 +13,6 @@ export const router = createBrowserRouter([
     {path: "/Consolidated_Loan", element: <Consolidated_Loan/>},
     {path: "/Emergency_Loan", element: <Emergency_Loan/>},
     {path: "/Sign_In", element: <Sign_In/>},
+    {path: "/Login", element: <Login/>},
 
 ])
