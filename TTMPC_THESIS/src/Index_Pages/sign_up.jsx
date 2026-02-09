621cd328df3a0546044e1602d0604418c5bded70
Gtabs2004 <geroantoni.tabiolo@wvsu.edu.ph>
2026-02-09 16:20:30 +0800

    testing

--- TTMPC_THESIS/src/Index_Pages/sign_up.jsx
+++ TTMPC_THESIS/src/Index_Pages/sign_up.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { UserAuth } from '../contex/AuthContext';
 
-export default function sign_up() {
+const Sign_Up = () =>{
+
+  const [username, setUsername] = useState ("");
+  const [password, setPassword] = useState ("");
+  const [error, setError] = useState ("");
+  const [loading, setLoading] = useState ("");
+
+   
+
   return (
-    <div>sign_up</div>
-  )
+    <div className="bg-white flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          src="src/assets/img/ttmpc logo.png"
+          alt="Your Company"
+          className="ml-32 my-auto h-30 w-auto"
+        />
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-green-700">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="h-80 bg-gray-100 p-12 mt-5 rounded-xl shadow-md sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleSubmit} className="space-y-6">
+          
+          {/* USERNAME FIELD */}
+          <div>
+            <label htmlFor="username" className="block text-sm/6 font-medium text-black">
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                id="username"
+                name="username"
+                type="text"
+                className="block w-full rounded-md bg-gray-400 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          {/* PASSWORD FIELD */}
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm/6 font-medium text-black">
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autoComplete="current-password"
+                value={formData.password}
+                onChange={handleChange}
+                className="block w-full rounded-md bg-gray-400 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              disabled={loading}
+              className="flex w-full justify-center rounded-md bg-green-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-14 text-center text-sm/6 text-gray-400">
+          Not a member?{' '}
+          <a href="#" className="font-semibold text-green-700">
+            Start a 14 day free trial
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
+
+export default Sign_Up;
--- TTMPC_THESIS/src/Router.jsx
+++ TTMPC_THESIS/src/Router.jsx
@@ -3,7 +3,7 @@ import App from "./App";
 import Bonus_Loan from "./LOANFORMS/Bonus_Loan";
 import Consolidated_Loan from "./LOANFORMS/Consolidated_Loan";
 import Emergency_Loan from "./LOANFORMS/Emergency_Loan";
-import Sign_In from "./Index_Pages/Sign_in";
+import Sign_Up from "./Index_Pages/Sign_Up";
 import Login from "./Index_Pages/login";
 
 
@@ -12,7 +12,7 @@ export const router = createBrowserRouter([
     {path: "/Bonus_Loan", element: <Bonus_Loan/>},
     {path: "/Consolidated_Loan", element: <Consolidated_Loan/>},
     {path: "/Emergency_Loan", element: <Emergency_Loan/>},
-    {path: "/Sign_In", element: <Sign_In/>},
+    {path: "/Sign_In", element: <Sign_Up/>},
     {path: "/Login", element: <Login/>},
 
 ])
