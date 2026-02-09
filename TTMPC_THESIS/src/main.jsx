5cdeec7364e645185cbc151aed58338de8700a8c
Gtabs2004 <geroantoni.tabiolo@wvsu.edu.ph>
2026-02-09 17:43:53 +0800

    updated

--- TTMPC_THESIS/src/main.jsx
+++ TTMPC_THESIS/src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router.jsx'
+import { AuthContextProvider } from "./contex/AuthContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <RouterProvider router={router} />
+     <AuthContextProvider>
+       <RouterProvider router={router} />
+     </AuthContextProvider>
+     
     </>
   </StrictMode>
 )
