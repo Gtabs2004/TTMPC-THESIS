182973f2313a41bef8f346c1e86b6c914db73d66
gtabs2004 <geroantoni.tabiolo@wvsu.edu.ph>
2026-09-02 10:35:57 +0800

    change the pagination from 10 items to 5 items

--- TTMPC_THESIS/src/Bookkeeper/Components/Bookkeeper_CBU.jsx
+++ TTMPC_THESIS/src/Bookkeeper/Components/Bookkeeper_CBU.jsx
@@ -21,7 +21,7 @@ import { PortalTopbarIdentity } from "../../components/PortalIdentity";
 import InterestOnShareCapitalModal from "../../components/InterestOnShareCapitalModal";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 5;
 
 const formatCurrency = (value) =>
   `₱${Number(value || 0).toLocaleString(undefined, {
